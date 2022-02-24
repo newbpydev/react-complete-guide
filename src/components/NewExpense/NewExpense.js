5f8113a2fd7c6f2ffdea1453334a61bcd1cde71c
newbpydev <newbpydev@gmail.com>
2022-02-24 19:34:40 -0400

    feat: add new expense  and cancel button to form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import NewExpenseForm from "./NewExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
-    }
+    };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+
+  function stopEditingHandler() {
+    setIsEditing(false);
   }
 
   return (
     <div className="new-expense">
-      <NewExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing ? (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      ) : (
+        <NewExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
-  )
- };
+  );
+};
 
 export default NewExpense;
--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./NewExpenseForm.css";
 
-const NewExpenseForm = ( props) => {
+const NewExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [isFormActive, setIsFormActive] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -16,21 +17,87 @@ const NewExpenseForm = ( props) => {
     setEnteredDate(event.target.value);
   };
 
-  const submitHandler = (event) => { 
+  const submitHandler = (event) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
-    }
+    };
     props.onSaveExpenseData(expenseData);
 
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+
+    setIsFormActive(false);
+  };
+
+  // const form = (
+  //   <form action="" onSubmit={submitHandler}>
+  //     <div className="new-expense__controls">
+  //       <div className="new-expense__control">
+  //         <label htmlFor="expense-title">Title</label>
+  //         <input
+  //           onChange={titleChangeHandler}
+  //           value={enteredTitle}
+  //           type="text"
+  //           name="expense-title"
+  //           id="expense-title"
+  //           required
+  //         />
+  //       </div>
+  //       <div className="new-expense__control">
+  //         <label htmlFor="expense-amount">Amount</label>
+  //         <input
+  //           onChange={amountChangeHandler}
+  //           value={enteredAmount}
+  //           type="number"
+  //           name="expense-amount"
+  //           id="expense-amount"
+  //           min="0.01"
+  //           step="0.01"
+  //           required
+  //         />
+  //       </div>
+  //       <div className="new-expense__control">
+  //         <label htmlFor="expense-date">Date</label>
+  //         <input
+  //           onChange={dateChangeHandler}
+  //           value={enteredDate}
+  //           type="date"
+  //           name="expense-date"
+  //           id="expense-date"
+  //           min="2019-01-01"
+  //           max="2023-12-31"
+  //           required
+  //         />
+  //       </div>
+
+  //       <div className="new-expense__actions">
+  //         <button type="submit">Cancel</button>
+  //       </div>
+  //       <div className="new-expense__actions">
+  //         <button type="submit">Add Expense</button>
+  //       </div>
+  //     </div>
+  //   </form>
+  // );
+
+  const btnAddExpense = (
+    <div className="new-expense__actions">
+      <button type="button" onClick={handleAddNewExpense}>
+        Add New Expense
+      </button>
+    </div>
+  );
+
+  function handleAddNewExpense(event) {
+    setIsFormActive((prevIsFormActive) => !prevIsFormActive);
   }
 
   return (
+    // <div>{isFormActive ? form : btnAddExpense}</div>
     <form action="" onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
@@ -71,6 +138,11 @@ const NewExpenseForm = ( props) => {
           />
         </div>
 
+        <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>
+            Cancel
+          </button>
+        </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
         </div>
